f443c826b8668ec0bfebcb18deef288602cbd0e8
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-12-27 18:27:38 +0545

    Update package.json and src/Pages/Home.jsx.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        Popop
+      </div> */}
+      <Canvas />
+    </section>
+  );
 };
 
 export default Home;
