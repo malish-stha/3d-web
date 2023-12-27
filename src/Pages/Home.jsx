d6c10e0e7a17165f878a8f163d08e962259a629b
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-12-27 20:18:11 +0545

    Update package.json, package-lock.json, Home.jsx, Island.jsx, and vite.config.js.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../Components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
+    let islandRotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+    return [screenScale, screenPosition, islandRotation];
+  };
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -12,7 +28,15 @@ const Home = () => {
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <ambientLight />
+          <spotLight />
+          <directionalLight />
+          <pointLight />
+          <hemisphereLight />
+
+          <Island position={islandPosition} scale={islandScale} />
+        </Suspense>
       </Canvas>
     </section>
   );
--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -6,24 +6,25 @@ Source: https://sketchfab.com/3d-models/volcano-island-lowpoly-4a6591dc9fee40d8b
 Title: Volcano Island Lowpoly
 */
 
-import React, { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { a } from "@react-spring/three";
+
+import islandScene from "../assets/3D/island.glb";
 
 const Island = (props) => {
-  const { nodes, materials } = useGLTF("/volcano_island_lowpoly.glb");
+  const islandRef = useRef();
+  const { nodes, materials } = useGLTF(islandScene);
   return (
-    <group {...props} dispose={null}>
+    <a.group ref={islandRef} {...props}>
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Clouds_Clouds_0.geometry}
         material={materials.Clouds}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.hammock_hammock_0.geometry}
         material={materials.hammock}
         position={[180.509, 1151.676, 5904.325]}
@@ -31,8 +32,6 @@ const Island = (props) => {
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Tequila_Bottle_Tequila_Bottle_0.geometry}
         material={materials.Tequila_Bottle}
         position={[3492.547, 1112.371, 3620.925]}
@@ -40,8 +39,6 @@ const Island = (props) => {
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.skeleton_skeleton_0.geometry}
         material={materials.skeleton}
         position={[3541.67, 1110.094, 3568.021]}
@@ -49,94 +46,72 @@ const Island = (props) => {
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Pyramid_Pyramid_0.geometry}
         material={materials.Pyramid}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Island_Grass_Island_Grass_0.geometry}
         material={materials.Island_Grass}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100.041}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.shrubbery_shrubbery_0.geometry}
         material={materials.shrubbery}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Lava_bubble_Lava_bubble_0.geometry}
         material={materials.Lava_bubble}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Volcanic_lava_Volcanic_lava_0.geometry}
         material={materials.Volcanic_lava}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Palm_tree_2_Palm_tree_2_0.geometry}
         material={materials.Palm_tree_2}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Palm_tree_1_Palm_tree_1_0.geometry}
         material={materials.Palm_tree_1}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Volacano_Sand_Volacano_Sand_0.geometry}
         material={materials.Volacano_Sand}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Ocean_Ocean_0.geometry}
         material={materials.Ocean}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Volcano_Grass_Volcano_Grass_0.geometry}
         material={materials.Volcano_Grass}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.Volcano_Base_Volcano_Base_0.geometry}
         material={materials.Volcano_Base}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={100.041}
       />
-    </group>
+    </a.group>
   );
 };
 
