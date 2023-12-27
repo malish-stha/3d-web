7997e546fb9352eb2741d232e4cba4acaab1d50b
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-12-27 17:55:59 +0545

    Update the navigation bar and added pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Homepage from "./Pages/Homepage";
+import ContactPage from "./Pages/ContactPage";
+import ProjectsPage from "./Pages/ProjectsPage";
+import AboutPage from "./Pages/AboutPage";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        Navbar
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </main>
