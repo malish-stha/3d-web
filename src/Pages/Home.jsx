90d8570c6bd01b4c3df2a3b1b4342d5831226cad
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-12-27 18:44:31 +0545

    Imported 3D Model.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../Components/Loader";
 
 const Home = () => {
   return (
@@ -6,7 +8,12 @@ const Home = () => {
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         Popop
       </div> */}
-      <Canvas />
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}></Suspense>
+      </Canvas>
     </section>
   );
 };
